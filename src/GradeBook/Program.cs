3be356130b46db36e3fa703498ed1af7ef8f9478
BecaLParker <becaparker@gmail.com>
2021-08-03 13:03:47 +0100

    Refactor to use array initialisation expression

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,8 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new double[3];
-            numbers[0] = 12.7;
-            numbers[1] = 15.1;
-            numbers[2] = 10.2;
+            var numbers = new double[] { 12.7, 15.1, 10.2 };
+
             var result = numbers[0] + numbers[1] + numbers[2];
             Console.WriteLine(result);
 
