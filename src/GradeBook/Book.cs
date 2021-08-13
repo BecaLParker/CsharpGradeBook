bea5372ab4203a7560625ef5a2df1872b2dfe7de
BecaLParker <becaparker@gmail.com>
2021-08-13 12:47:39 +0100

    Extracts statistical functions to Book class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -16,11 +17,28 @@ namespace GradeBook
 
         }
 
+        public void ShowStatistics()
+        {
+            var total = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                total += number;
+            }
+
+            var average = total / grades.Count;
+
+            Console.WriteLine($"Average = {average:N1}");
+            Console.WriteLine($"Highest grade = {highGrade:N1}");
+            Console.WriteLine($"Lowest grade = {lowGrade:N1}");
+        }
+
         // fields (instance variables?)
         // make private so we can add logic validatation in class methods to prevent out-of-scope grades (eg 101) being added via grades.Add
         private List<double> grades;
         private string name;
-
-
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,25 +10,9 @@ namespace GradeBook
         {
             var book = new Book("Yr7 Science Grades");
             book.AddGrade(89.1);
-
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-
-            var total = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach (double number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                total += number;
-            }
-
-            var average = total / grades.Count;
-
-            Console.WriteLine($"Average = {average:N1}");
-            Console.WriteLine($"Highest grade = {highGrade:N1}");
-            Console.WriteLine($"Lowest grade = {lowGrade:N1}");
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
         }
     }
 }
