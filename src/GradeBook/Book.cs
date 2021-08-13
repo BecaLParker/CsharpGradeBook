8023907d1927d6c51ffeb348c935545f590bd483
BecaLParker <becaparker@gmail.com>
2021-08-13 12:10:16 +0100

    Grades are a state of Book class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        //  Constructor method (~ Ruby def initialize)
+        public Book()
+        {
+            grades = new List<double>();
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+
+        }
+
+        // field (instance variable?)
+        List<double> grades;
+
 
     }
 }
