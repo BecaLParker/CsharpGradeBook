5e2fe4459dab086c693b6642d54e8d22336200cc
BecaLParker <becaparker@gmail.com>
2021-08-15 12:40:27 +0100

    Added project reference between testing adn GradeBook

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book
+    public class Book
     {
         //  Constructor method (~ Ruby def initialize)
         public Book(string name)
